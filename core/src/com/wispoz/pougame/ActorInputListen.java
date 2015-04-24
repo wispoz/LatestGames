36e478cb4b190da6aa6d9b343cd7769b8d97f7de
wispoz <wispoz@inbox.ru>
2015-04-25 01:54:04 +0300

    Buttons Itemss

--- core/src/com/wispoz/pougame/ActorInputListen.java
+++ core/src/com/wispoz/pougame/ActorInputListen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.uwsoft.editor.renderer.actor.CompositeItem;
+import java.lang.String;
 
 /**
  * Created by wispoz on 20.04.15.
@@ -16,7 +17,12 @@ public class ActorInputListen extends ActorGestureListener {
       //  super(0.05f, 0.4f, 1.1f, 0.15f);
         game = cell;
     }
+/*
 
+
+6 7
+10 11
+ */
     @Override
     public void fling (InputEvent event, float velocityX, float velocityY, int button) {
 
@@ -26,11 +32,13 @@ public class ActorInputListen extends ActorGestureListener {
                 CompositeItem moveTo = scene.getCompositeById("grid_button16");
                 Float x = moveTo.getX();
                 Float y = moveTo.getY();
-                Float bindedGrid = game.getCustomVariables().getFloatVariable("bind");
-                if(bindedGrid == 15) {
-                  //  game.getCustomVariables().setVariable("bind","16.0");
+                Float binded = game.getCustomVariables().getFloatVariable("bind");
 
-                    game.addAction(Actions.moveTo( x, y, 0.7f, Interpolation.bounceOut));
+                String bindedGrid = binded.toString();
+                if(bindedGrid.equals("15.0")) {
+                    game.getCustomVariables().setVariable("bind","16.0");
+                    moveTo.getCustomVariables().setVariable("free","1");
+                    game.addAction(Actions.moveTo( x, y, 0.7f, Interpolation.swingOut));
                 }
 
 
@@ -43,11 +51,13 @@ public class ActorInputListen extends ActorGestureListener {
                 Float x = moveTo.getX();
                 Float y = moveTo.getY();
                 String bindedGrid = game.getCustomVariables().getFloatVariable("bind").toString();
-                game.addAction(Actions.moveTo( x, y, 0.7f, Interpolation.bounceOut));
-/*                if(bindedGrid == "16.0") {
-                    game.getCustomVariables().setVariable("bind","15.0");
-                    game.addAction(Actions.moveTo( x, y, 0.7f, Interpolation.bounceOut));
-                }*/
+             //   game.addAction(Actions.moveTo( x, y, 0.7f, Interpolation.bounceOut));
+                System.out.println("SWIPE: UP" + bindedGrid);
+               if(bindedGrid.equals("16.0")) {
+                  game.getCustomVariables().setVariable("bind","15.0");
+                  moveTo.getCustomVariables().setVariable("free","1");
+                  game.addAction(Actions.moveTo( x, y, 0.7f, Interpolation.bounceOut));
+                }
             }
         }else{
             if(velocityY>0){
