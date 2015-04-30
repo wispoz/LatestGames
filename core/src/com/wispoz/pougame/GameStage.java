c81a2316e2a0f7b0293c22b7f1896c1f50a8c539
wispoz@inbox.ru <fallout12252240>
2015-04-30 18:56:30 +0400

    last edition

--- core/src/com/wispoz/pougame/GameStage.java
+++ core/src/com/wispoz/pougame/GameStage.java
@@ -31,14 +31,14 @@ public class GameStage extends Overlap2DStage {
     public GameStage() {
         //super(new StretchViewport(rm.stageWidth, rm.currentResolution.height));
         Gdx.input.setInputProcessor(this);
-        rm = new ResolutionManager();
-        rm.initGameResources();
-        /*
+       // rm = new ResolutionManager();
+      //  rm.initGameResources();
+
         // This will create SceneLoader instance and configure all things like default resource manager, physics e.t.c
        //initSceneLoader(rm);
      //   sceneLoader.setResolution(rm.currentResolution.name);
         resourceManager = new ResourceManager();
-      // resourceManager.setWorkingResolution("1080x1920");
+       resourceManager.setWorkingResolution("480x800");
         			// loading assets into memory
 
 
@@ -51,44 +51,47 @@ public class GameStage extends Overlap2DStage {
         // Adding root actor to stage
         addActor(menuLoader.sceneActor);
 
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button1"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button2"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button3"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button4"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button5"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button6"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button7"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button8"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button9"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button10"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button11"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button12"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button13"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button14"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button15"));
-        gridsList.add(menuLoader.sceneActor.getCompositeById("grid_button16"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid1"));
 
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button1"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button2"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button3"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button4"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button5"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button6"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button7"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button8"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button9"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button10"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button11"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button12"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button13"));
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button14"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid2"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid3"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid4"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid5"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid6"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid7"));
 
-        buttonsList.add(menuLoader.sceneActor.getCompositeById("button15"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid8"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid9"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid10"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid11"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid12"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid13"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid14"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid15"));
+        gridsList.add(menuLoader.sceneActor.getCompositeById("grid16"));
+
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button1"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button2"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button3"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button4"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button5"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button6"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button7"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button8"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button9"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button10"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button11"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button12"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button13"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button14"));
+        buttonsList.add(menuLoader.sceneActor.getCompositeById("grid_button15"));
         for(CompositeItem item: buttonsList) {
                 item.addScript(new GameItem());
         }
+
         Collections.shuffle(buttonsList);
         int index = 0;
+
         for(CompositeItem item: gridsList) {
             float x = item.getX();
             float y = item.getY();
@@ -96,11 +99,11 @@ public class GameStage extends Overlap2DStage {
                 CompositeItem button = buttonsList.get(index);
                 String id = item.getCustomVariables().getFloatVariable("id").toString();
                 button.getCustomVariables().setVariable("bind",id);
-                button.addAction(Actions.moveTo(x, y, 1.0f, Interpolation.bounceOut));
+                button.addAction(Actions.moveTo(x, y, 1.0f, Interpolation.exp10));
             }
             index++;
         }
-        */
+
         /*
         for(IBaseItem item: menuLoader.getRoot().getItems()) {
            if( item.isComposite()){
